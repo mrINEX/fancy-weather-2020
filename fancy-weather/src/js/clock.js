b60748f0c223ab21f56fbd2a824a88126cf85c8d
mrinex <mrinex@mail.ru>
2020-05-20 09:10:46 +0300

    feat: add switch next day clock

--- fancy-weather/src/js/clock.js
+++ fancy-weather/src/js/clock.js
@@ -2,10 +2,21 @@ function clock() {
   const secondsSpan = document.querySelector('.current-time').children[2];
   if ((Number(secondsSpan.textContent) + 1) % 60 === 0) {
     const minutesSpan = document.querySelector('.current-time').children[1];
+    const hourseSpan = document.querySelector('.current-time').children[0];
     if ((Number(minutesSpan.textContent) + 1) % 60 < 10) {
       minutesSpan.textContent = `0${(Number(minutesSpan.textContent) + 1) % 60}`;
     } else {
       minutesSpan.textContent = (Number(minutesSpan.textContent) + 1) % 60;
+    }
+    if ((Number(minutesSpan.textContent)) % 60 === 0) {
+      if ((Number(hourseSpan.textContent) + 1) % 24 < 10) {
+        hourseSpan.textContent = `0${(Number(hourseSpan.textContent) + 1) % 24}`;
+      } else {
+        hourseSpan.textContent = (Number(hourseSpan.textContent) + 1) % 24;
+      }
+      if ((Number(hourseSpan.textContent)) % 24 === 0) {
+        console.log('next day !!!');
+      }
     }
   }
   if ((Number(secondsSpan.textContent) + 1) % 60 < 10) {
